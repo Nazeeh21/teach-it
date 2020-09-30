ad435dceaa992aa5ec479928aa8550364efea153
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2020-10-01 03:54:20 +0530

    Refine UI

--- components/Inputs/SearchBar.js
+++ components/Inputs/SearchBar.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import { Icon } from '../Images/Icon'
 
 const SearchBar = ({ value, changeHandler }) => {
   const router = useRouter()
 
   return (
-    <input
-      onKeyUp={(e) => {
-        if (e.key === 'Enter') {
-          router.push('/search')
-        }
-      }}
-      style={{ outline: 'none' }}
-      value={value}
-      onChange={(e) => changeHandler(e.target.value)}
-      className='rounded-full bg-white pt-2 pb-2 pl-4 pr-4 w-full-'
-    />
+    <div className='flex'>
+      <input
+        onKeyUp={(e) => {
+          if (e.key === 'Enter') {
+            router.push('/search')
+          }
+        }}
+        style={{ outline: 'none' }}
+        value={value}
+        onChange={(e) => changeHandler(e.target.value)}
+        className='rounded-full bg-white pt-2 pb-2 pl-4 pr-4 w-full'
+      />
+      <div onClick={() => router.push('/search')} className='-ml-12'>
+        <Icon src='search.png' />
+      </div>
+    </div>
   )
 }
 
