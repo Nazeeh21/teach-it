f4bfd1c056294a919f754d81e6b7e009738fd485
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-10-01 00:33:52 +0530

    Make half Landing page responsive

--- components/TopNavbar/LandingPageNavbar/LandingPageNavbar.js
+++ components/TopNavbar/LandingPageNavbar/LandingPageNavbar.js
@@ -6,20 +6,21 @@ const LandingPageNavbar = () => {
   const router = useRouter()
 
   return (
-    <div className='px-32 pt-12 justify-center bg-white w-full grid grid-cols-2 grid rows-1'>
-      <div className='flex items-center text-lg text-darkGrey font-medium'>
-        <img onClick={() => router.push('/')} className='cursor-pointer w-64' src='Logo.svg' alt='logo' />
-        <p style={{ cursor: 'pointer' }} className='ml-10'>
+    <div className='px-3 pt-2 lg:px-32 lg:pt-12 lg:justify-center bg-white w-full lg:grid lg:grid-cols-2 lg:grid-rows-1'>
+      <div className='grid grid-cols-2 lg:flex lg:justify-items-center lg:items-center text-lg text-darkGrey font-medium w-full'>
+        <img onClick={() => router.push('/')} className='cursor-pointer' src='Logo.svg' alt='logo' />
+        <p style={{ cursor: 'pointer' }} className='hidden lg:ml-10 lg:block'>
           Home
         </p>
-        <p style={{ cursor: 'pointer' }} className='ml-10'>
+        <p style={{ cursor: 'pointer' }} className='hidden lg:ml-10 lg:block'>
           Features
         </p>
-        <p style={{ cursor: 'pointer' }} className='ml-10'>
+        <p style={{ cursor: 'pointer' }} className='hidden lg:ml-10 lg:block'>
           Contact us
         </p>
+        <div className='justify-self-end cursor-pointer lg:hidden'><img src='hand-burger.svg' alt='hand-burger' /></div>
       </div>
-      <div className='justify-self-end'>
+      <div className='hidden justify-self-end lg:block'>
         <SecondaryButton label='Get Started' clickHandler={() => router.push('/login')} />
       </div>
     </div>
