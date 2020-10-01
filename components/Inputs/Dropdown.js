663e717f0ce4be7755306627cbfe7e98a3332855
Dhaiwat10 <dhaiwatpandya@gmail.com>
2020-10-02 02:50:20 +0530

    Add profile settings

---
+++ components/Inputs/Dropdown.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Dropdown = () => {
+  return (
+    <select className='bg-highlight p-4 rounded'>
+      <option>Test</option>
+      <option>Test</option>
+      <option>Test</option>
+      <option>Test</option>
+      <option>Test</option>
+    </select>
+  )
+}
+
+export default Dropdown
--- components/Nav/InnerNav/index.js
+++ components/Nav/InnerNav/index.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import NavItem from './NavItem'
 
-const Index = ({ navItems }) => {
+const Index = ({ navItems, destructiveAction = 'Unsubscribe' }) => {
+  // return (
+  //   <div className='bg-white rounded-lg pt-4 pb-4'>
+  //     <NavItem label='Service' active />
+  //     <NavItem label='Service information' />
+  //     <NavItem label='Questions' />
+  //     <p className='text-red text-sm cursor-pointer text-right pr-12 pb-2 pt-2'>Unsubscribe</p>
+  //   </div>
+  // )
+
   return (
     <div className='bg-white rounded-lg pt-4 pb-4'>
       <NavItem label='Service' active />
-      <NavItem label='Service information' />
-      <NavItem label='Questions' />
-      <p className='text-red text-sm cursor-pointer text-right pr-12 pb-2 pt-2'>Unsubscribe</p>
+      <NavItem label='General' />
+      <NavItem label='App' />
+      <NavItem label='Payments' />
+      <NavItem label='Profile' />
+      <p className='text-red text-sm cursor-pointer text-right pr-12 pb-2 pt-2'>Disable account</p>
     </div>
   )
 };
