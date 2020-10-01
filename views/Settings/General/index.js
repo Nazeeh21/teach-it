74f415797f8b921aa5dba1031aef232e6e820504
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-10-02 00:10:24 +0530

    Complete General Settings component

--- views/Settings/General/index.js
+++ views/Settings/General/index.js
@@ -1,14 +1,58 @@
 import React from 'react'
+import { shallowEqual } from 'react-redux'
+import { PrimaryButton } from '../../../components/Buttons/Index'
+import Pills3 from '../../../components/Misc/3Pills/3Pills'
+import ShowAllButton from '../ShowAllButton'
 import User from '../Users'
+
+const Unblock = () => <div style={{color: '#4968FF'}} className='cursor-pointer text-md'>Unblock</div>
+
+const GreyLine = () => <div style={{height: '0.05rem', opacity: '50%'}} className='bg-darkGrey w-full' />
+
+const AcceptedText = () => <div className='text-md text-darkGrey'>Accepted on 13/6</div>
+
+const PendingText = () => <div style={{color: '#FF7575'}} className='text-md'>Pending</div>
 
 const GeneralSetting = () => (
   <div className='w-full'>
     <p className='text-2xl font-medium'>General settings</p>
-    <div className='w-full h-auto pt-6 px-5 bg-white rounded-lg'>
-      <p className='text-lg'>Blocked users</p>
-      <div className='w-full bg-lightGrey'>
-        <User src
+    <div className='w-full h-auto mt-5 pt-6 px-5 bg-white rounded-lg'>
+      <p className='text-lg font-medium'>Blocked users</p>
+      <div className='w-full bg-lightGrey mt-6 pb-4'>
+        <User src='nazeeh_profile.jpg' name='Arun P' text={<Unblock />} />
+        <GreyLine />
+        <User src='nazeeh_profile.jpg' name='Arun P' text={<Unblock />} />
+        <GreyLine />
+        <User src='nazeeh_profile.jpg' name='Arun P' text={<Unblock />} />
+        <GreyLine />
+        <User src='nazeeh_profile.jpg' name='Arun P' text={<Unblock />} />
+        <ShowAllButton />
       </div>
+      <div className='w-full mt-6 grid grid-rows-1 grid-cols-2 items-center'>
+        <p className='text-lg font-medium'>Invitations</p>
+        <div>
+          <Pills3 width='full' color='white' label1='All' label2='Accepted' label3='Pending' />
+        </div>
+      </div>
+      <div className='w-full bg-lightGrey mt-4 pb-4'>
+        <User src='nazeeh_profile.jpg' name='Arun P' text={<AcceptedText />} />
+        <GreyLine />
+        <User src='nazeeh_profile.jpg' name='Arun P' text={<AcceptedText />} />
+        <GreyLine />
+        <User src='nazeeh_profile.jpg' name='Arun P' text={<PendingText />} />
+        <GreyLine />
+        <User src='nazeeh_profile.jpg' name='Arun P' text={<AcceptedText />} />
+        <ShowAllButton />
+      </div>
+      <p className='text-lg font-medium mt-8'>Preferred Language</p>
+      <div className='w-full mt-3'>
+        <select className='bg-lightGrey w-5/12 py-2 pl-1'>
+          <option value='english' label='English' />
+          <option value='hindi' label='Hindi' />
+          <option value='german' label='German' />
+        </select>
+      </div>
+      <div className='mt-8 w-4/12'><PrimaryButton label='Save' /></div>
     </div>
   </div>
 )
--- views/Settings/Users/index.js
+++ views/Settings/Users/index.js
@@ -3,18 +3,21 @@ import Avatar from '../../../components/Images/Avatar'
 
 const Option = () => (
   <div className='flex'>
-    <div className='bg-darkGrey w-3 h-3 '
+    <div className='rounded-full bg-darkGrey w-1 h-1 mr-1' />
+    <div className='rounded-full bg-darkGrey w-1 h-1 mr-1' />
+    <div className='rounded-full bg-darkGrey w-1 h-1 mr1' />
   </div>
 )
 
 const User = ({ src, name, text }) => (
-  <div className='w-full bg-lightGrey text-darkGrey grid grid-cols-2 grid-rows-1'>
-    <div className='flex'>
+  <div className='w-full bg-lightGrey py-1 px-2 text-darkGrey grid grid-cols-2 grid-rows-1 items-center'>
+    <div className='flex items-center'>
       <Avatar src={src} alt={name} purpose='isForChat' />
-      <p className='text-lg'>{name}</p>
+      <p className='text-lg ml-2'>{name}</p>
     </div>
-    <div className='flex justify-end'>
+    <div className='flex items-center justify-end'>
       <div>{text}</div>
+      <div className='cursor-pointer mx-3'>{<Option />}</div>
     </div>
   </div>
 )
