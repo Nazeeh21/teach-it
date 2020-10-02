b3762ae73a77868c0ec553f90646abc0b94bbf70
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-10-02 16:17:24 +0530

    Improve OptionCOntainer alignment

--- views/Settings/Users/index.js
+++ views/Settings/Users/index.js
@@ -13,21 +13,22 @@ const Option = () => (
 const User = ({ src, name, text }) => {
   const [showOptions, setShowOptions] = useState(false)
 
-  const openShowOptions = () => setShowOptions(true)
-
-  const closeShowOptions = () => setShowOptions(false)
+   const toggleShowOptionsHandler = () => setShowOptions(prevState => !prevState)
 
   return (
     <div className='w-full bg-lightGrey py-1 px-2 text-darkGrey grid grid-rows-2 grid-cols-1 md:grid-cols-2 md:grid-rows-1 items-center'>
-      
-        <OptionContainer show={showOptions} clickHandler={closeShowOptions} /> 
+      {showOptions && <div style={{position: 'absolute', marginLeft: '32rem', marginTop: '2.5rem', zIndex: '2000'}} className=''><OptionContainer /> </div>}
       <div className='flex items-center'>
         <Avatar src={src} alt={name} purpose='isForChat' />
         <p className='mdtext-lg ml-2'>{name}</p>
       </div>
       <div className='flex items-center justify-end'>
         <div>{text}</div>
-        <div className='cursor-pointer mx-3' onClick={openShowOptions}>{<Option />}</div>
+        <div>
+          <div className='cursor-pointer mx-3' onClick={toggleShowOptionsHandler}>{<Option />}</div>
+          
+        </div>
+        
       </div>
     </div>
   )
