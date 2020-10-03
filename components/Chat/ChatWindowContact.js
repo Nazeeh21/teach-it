1c547829e70204c80ab2909fd41e549e8dd77f64
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-10-03 13:41:57 +0530

    Add dynamic current state on click in CHatWindowCOmponent

--- components/Chat/ChatWindowContact.js
+++ components/Chat/ChatWindowContact.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Avatar from '../Images/Avatar'
 
 export const ChatWindowContact = ({src, name, text, active = false, time, current = false, clickHandler}) => (
-  <div style={{borderBottom: '2px solid #f3f4f8'}} className={`bg-${current ? 'lightCyan' : 'white'} cursor-pointer p-2 flex flex-row items-center w-full rounded-b-lg`}>
+  <div onClick={() => clickHandler(name)} style={{borderBottom: '2px solid #f3f4f8'}} className={`bg-${current ? 'lightCyan' : 'white'} cursor-pointer p-2 flex flex-row items-center w-full rounded-b-lg`}>
     <Avatar src={src} alt={name} purpose='isForChat' />
-    <div onClick={() => clickHandler(name)}>
+    <div>
       <p className={`${current ? 'font-bold' : 'font-normal'} text-xl`}>{name}</p>
       <div className='grid grid-cols-2 items-center w-full'>
         <p className='text-lg text-darkGrey justify-start mr-4'>{text}</p>
