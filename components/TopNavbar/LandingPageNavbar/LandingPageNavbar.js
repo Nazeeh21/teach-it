91d835609c5e6b1b76a677f1d3ce5cd982978446
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-10-05 23:02:03 +0530

    Add Contact-us component

--- components/TopNavbar/LandingPageNavbar/LandingPageNavbar.js
+++ components/TopNavbar/LandingPageNavbar/LandingPageNavbar.js
@@ -12,7 +12,7 @@ const LandingPageNavbar = () => {
 
   
   return (
-    <div className='px-3 pt-2 md:px-5 mx:pt-5 lg:px-32 lg:pt-12 bg-white w-full md:grid md:grid-cols-2 md:grid-rows-1 '>
+    <div className='px-3 pt-2 md:px-5 mx:pt-5 lg:px-32 lg:pt-12 bg-none w-full md:grid md:grid-cols-2 md:grid-rows-1 '>
       <SideDrawer open={openSideDrawer} closed={toggleSideDrawerHandler} />
       <div className='grid grid-cols-2 md:flex md:justify-items-center md:items-center text-md lg:text-lg text-darkGrey font-medium w-full'>
         <img onClick={() => router.push('/')} className='cursor-pointer' src='/Logo.svg' alt='logo' />
@@ -22,7 +22,7 @@ const LandingPageNavbar = () => {
         <a href='/' style={{ cursor: 'pointer' }} className='hidden md:ml-4 md:w-auto lg:ml-10 md:block'>
           Features
         </a>
-        <a href='/' style={{ cursor: 'pointer' }} className='hidden md:ml-4 md:w-auto lg:ml-10 md:block'>
+        <a href='/contact-us' style={{ cursor: 'pointer' }} className='hidden md:ml-4 md:w-auto lg:ml-10 md:block'>
           Contact us
         </a>
         <div className='justify-self-end cursor-pointer md:hidden' onClick={toggleSideDrawerHandler}><img src='/hand-burger.svg' alt='hand-burger' /></div>
---
+++ pages/contact-us/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import ContactUs from '../../views/ContactUs'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>VideoWork</title>
+        <link rel='icon' href='/favicon.ico' />
+        {/* <!-- Fonts --> */}
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+
+      <main>
+        <ContactUs />
+      </main>
+    </div>
+  )
+}
