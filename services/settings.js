21562d6b6a12bf203fb87cc3570d4fa30da8a7f1
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-10-28 18:58:20 +0530

    Implement milestone on view-service route

--- services/settings.js
+++ services/settings.js
@@ -14,3 +14,18 @@ export const fetchProfileData = async () => {
     return e
   }
 }
+
+export const fetchMilestone = async (id) => {
+  try {
+    const res = await api.get(`service/${id}/milestone/`, {
+      headers: {
+        Authorization: `Token ${localStorage.getItem('token')}`,
+        'X-Profile-ID': window.localStorage.getItem('currentProfile'),
+      },
+    })
+    console.log('fetchMilstone ', res.data)
+    return res.data
+  } catch (e) {
+    console.log(e)
+  }
+}
