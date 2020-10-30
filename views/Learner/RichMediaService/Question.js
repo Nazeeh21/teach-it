d7597e792b6fca31360ea210f2cb118aa0f1258f
Dhaiwat10 <dhaiwatpandya@gmail.com>
2020-10-30 17:36:43 +0530

    Add support tickets list display

--- views/Learner/RichMediaService/Question.js
+++ views/Learner/RichMediaService/Question.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { PrimaryButton } from '../../../components/Buttons/Index'
 
-const Question = ({ question, time, date, clickHandler }) => {
+const Question = ({ question, time, clickHandler }) => {
+  const date = new Date(time)
+
+  console.log(date)
+
   return (
     <React.Fragment>
       <div className='flex rounded-lg border-highlight border-2 bg-white w-full p-6'>
-        <p className='w-3/4 font-semibold'>{question}?</p>
+        <p className='w-3/4 font-semibold'>{question}</p>
         <div className='w-1/4 ml-4'>
           <PrimaryButton label='View' clickHandler={clickHandler} />
         </div>
       </div>
       <div className='grid grid-cols-1 -mt-5 font-semibold'>
-        <p className='justify-self-end text-darkGrey text-sm'>4.35pm, 26 September</p>
+        <p className='justify-self-end text-darkGrey text-sm'>{date.toString()}</p>
       </div>
       
     </React.Fragment>
--- views/Support/index.js
+++ views/Support/index.js
@@ -1,26 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../api'
 import { PrimaryButton } from '../../components/Buttons/Index'
 import Pills3 from '../../components/Misc/3Pills/3Pills'
 import loremIpsum from '../../utility/loremIpsum'
 import Question from '../Learner/RichMediaService/Question'
 
-const Support = () => (
-  <div className='w-full'>
-    <p className='text-2xl font-medium'>Support</p>
-    <div className='grid grid-cols-1 grid-rows-1 lg:grid-cols-2 lg:girid-rows-1'>
-      <div className='hidden lg:block' />
-      <Pills3 color='white' width='w-full' label1='Open' label2='Closed' label3='Escalated' />
-    </div>
-    <div className='grid grid-flow-row grid-cols-1 gap-6 mt-6'>
-      <Question question={loremIpsum.substring(100)} />
-      <Question question={loremIpsum.substring(100)} />
-      <Question question={loremIpsum.substring(100)} />
-    </div>
-    <div className='mb-20 md:mb-0 grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 items-center mt-5'>
+const Support = () => {
+  const [filter, setFilter] = useState('open')
+  const [tickets, setTickets] = useState([])
+
+  const fetchTickets = async () => {
+    try {
+      const res = await api.get('support/', {
+        headers: {
+          'Authorization': `Token ${localStorage.getItem('token')}`,
+          'X-Profile-ID': localStorage.getItem('currentProfile')
+        }
+      })
+
+      console.log('Fetched support tickets', res.data)
+
+      return res.data
+    } catch (e) {
+      console.log('Error while fetching support tickets', e)
+      return null
+    }
+  }
+
+  useEffect(() => {
+    fetchTickets().then(res => setTickets(res)).catch(e => console.log(e))
+  }, [])
+
+  return (
+    <div className='w-full'>
+      <p className='text-2xl font-medium'>Support</p>
+      <div className='grid grid-cols-1 grid-rows-1 lg:grid-cols-2 lg:girid-rows-1'>
+        <div className='hidden lg:block' />
+        <Pills3 activeLabel={filter} setLabel={setFilter} color='white' width='w-full' label1='Open' label2='Closed' label3='Escalated' />
+      </div>
+      <div className='grid grid-flow-row grid-cols-1 gap-6 mt-6'>
+        {/* <Question question={loremIpsum.substring(100)} />
+        <Question question={loremIpsum.substring(100)} />
+        <Question question={loremIpsum.substring(100)} /> */}
+        {
+          tickets.filter(ticket => ticket.status.toLowerCase() === filter).map(ticket => <Question question={ticket.title} time={ticket.created_at} />)
+        }
+      </div>
+      <div className='mb-20 md:mb-0 grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 items-center mt-5'>
         <div className='w-full md:w-11/12'>
           <PrimaryButton label='New ticket' />
-          </div></div>
-  </div>
-)
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Support
