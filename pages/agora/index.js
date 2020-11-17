3f311aa22e01a93b0abf23f1f0a169a228ca3b42
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-11-17 23:49:22 +0530

    Add Input component

--- pages/agora/index.js
+++ pages/agora/index.js
@@ -28,13 +28,25 @@ const Index = () => {
             </div>
           </div>
           <div className='text-white flex-col w-full text-center mt-64'>
-            <div className='font-semibold'>You are the only person in the call</div>
-            <div className='text-xs font-medium'>We have notified the group</div>
-            <div className='bg-expert py-2 rounded-full w-2/12 m-auto mt-4'>Ring the group</div>
+            <div className='font-semibold'>
+              You are the only person in the call
+            </div>
+            <div className='text-xs font-medium'>
+              We have notified the group
+            </div>
+            <div className='bg-expert py-2 rounded-full w-2/12 m-auto mt-4'>
+              Ring the group
+            </div>
             <div className='flex items-center justify-center mt-4'>
-              <div className='w-12 cursor-pointer bg-white rounded-full p-2 h-auto'><img src='mic.png' alt='Mic' /></div>
-              <div className='w-12 cursor-pointer bg-white rounded-full p-2 h-auto mx-4'><img src='video-camera.png' alt='Video Camera' /></div>
-              <div className='w-8 cursor-pointer'><img src='endCall.png' alt='End call' /></div>
+              <div className='w-12 cursor-pointer bg-white rounded-full p-2 h-auto'>
+                <img src='mic.png' alt='Mic' />
+              </div>
+              <div className='w-12 cursor-pointer bg-white rounded-full p-2 h-auto mx-4'>
+                <img src='video-camera.png' alt='Video Camera' />
+              </div>
+              <div className='w-8 cursor-pointer'>
+                <img src='endCall.png' alt='End call' />
+              </div>
             </div>
           </div>
         </div>
@@ -44,6 +56,30 @@ const Index = () => {
         </div>
         <div>
           <VideoChat />
+          <div
+            className='bg-accentedWhite rounded-bl-md rounded-br-md flex justify-center w-6/12'
+            id='typebox'
+          >
+            <button
+              className='m-auto w-8 flex justify-center items-center'
+              onClick={() => {}}
+            >
+              <img src='/drawer.svg' alt='drawer' />
+            </button>
+            <button className='m-auto w-8 flex justify-center items-center'>
+              <img src='/camera.svg' alt='camera' />
+            </button>
+            <button className='m-auto w-8 flex justify-center items-center'>
+              <img src='/gallery.svg' alt='gallery' />
+            </button>
+            <input
+              style={{ outline: 'none' }}
+              className='border-highlight border-2 w-8/12 h-10 mx-2 my-2 rounded px-2 py-4'
+              type='text'
+              onChange={(e) => e.target.value}
+              // disabled={disabled}
+            />
+          </div>
         </div>
       </div>
     </LayoutProvider>
