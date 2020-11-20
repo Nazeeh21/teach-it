1f0182704af44c47bbd51faeee11bb42749acb48
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-11-20 20:03:59 +0530

    Integrate api calls on switching profile between Expert and Seeker

--- components/NavSwitch/index.js
+++ components/NavSwitch/index.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Pill } from '../Buttons/Index'
 import { useDispatch, useSelector } from 'react-redux'
 import { CHANGE_USER_TYPE } from '../../store/actionTypes'
 import { EXPERT, LEARNER } from '../../constants'
 import { changeUserType } from '../../store/actions/appActions'
+import {
+  isProvider,
+  isSeeker,
+  registerProvider,
+  registerSeeker,
+} from '../../services/profile'
 
 const Index = ({ color1, color2, label1, label2, textColor = 'white' }) => {
   const dispatch = useDispatch()
 
   const activeId = useSelector((state) => state.app.userType)
+
+  useEffect(() => {
+    activeId === EXPERT
+      ? isProvider().then((res) => {
+          // console.log('isProvider', res.isProvider)
+          if (res.isProvider === false) {
+            registerProvider(res.name)
+              .then((res) => console.log('Registering Provider', res))
+              .catch((e) => console.log(e))
+          }
+        })
+      : isSeeker().then((res) => {
+          // console.log('isSeeker', res.isSeeker)
+          if (res.isSeeker === false) {
+            registerSeeker(res.name)
+              .then((res) => console.log('Registering Seeker', res))
+              .catch((e) => console.log(e))
+          }
+        })
+  }, [activeId])
 
   const setActiveId = (index) => {
     dispatch(changeUserType(index))
--- services/profile.js
+++ services/profile.js
@@ -32,10 +32,96 @@ export const fetchAllProfiles = async () => {
 export const isProvider = async () => {
   // send get request to '/provider' and check if the current profile object has a field named provider
   // if exitsts then return true else return false
+  try {
+    const res = await api.get('user/', {
+      headers: {
+        Authorization: `Token ${localStorage.getItem('token')}`,
+        'X-Profile-ID': localStorage.getItem('currentProfile'),
+      },
+    })
+    // console.log('isProvider', res.data)
+    const currentProfileId = localStorage.getItem('currentProfile')
+    let currentProfileData = res.data.profiles.filter(
+      (profile) => profile.id == currentProfileId
+    )
+    console.log('isProvider in profile.js', currentProfileData)
+
+    return {
+      name: currentProfileData[0].name,
+      isProvider: 'provider' in currentProfileData,
+    }
+  } catch (e) {
+    console.log(e)
+  }
 }
 
-export const isSeeker = async () => {}
+export const isSeeker = async () => {
+  try {
+    const res = await api.get('user/', {
+      headers: {
+        Authorization: `Token ${localStorage.getItem('token')}`,
+        'X-Profile-ID': localStorage.getItem('currentProfile'),
+      },
+    })
+    // console.log('isSeeker', res.data)
+    const currentProfileId = localStorage.getItem('currentProfile')
+    let currentProfileData = res.data.profiles.filter(
+      (profile) => profile.id == currentProfileId
+    )
+    console.log('isSeeker in profile.js', currentProfileData)
 
-export const registerProvider = async () => {}
+    return {
+      name: currentProfileData[0].name,
+      isSeeker: 'seeker' in currentProfileData,
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
 
-export const registerSeeker = async () => {}
+export const registerProvider = async (name) => {
+  try {
+    const res = await api.put(
+      'provider/',
+      {
+        name: name,
+        country: 'India',
+      },
+      {
+        headers: {
+          Authorization: `Token ${localStorage.getItem('token')}`,
+          'X-Profile-ID': localStorage.getItem('currentProfile'),
+        },
+      }
+    )
+
+    console.log(res)
+    return res.data
+  } catch (e) {
+    // console.log(e)
+    return e
+  }
+}
+
+export const registerSeeker = async (name) => {
+  try {
+    const res = await api.put(
+      'seeker/',
+      {
+        name: name,
+      },
+      {
+        headers: {
+          Authorization: `Token ${localStorage.getItem('token')}`,
+          'X-Profile-ID': localStorage.getItem('currentProfile'),
+        },
+      }
+    )
+
+    console.log(res)
+    return res.data
+  } catch (e) {
+    // console.log(e)
+    return e
+  }
+}
