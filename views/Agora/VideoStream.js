18d1b1f885550623790f4a058eaf49dc3309fd08
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-12-13 19:50:56 +0530

    Add Dynamic key

--- views/Agora/VideoStream.js
+++ views/Agora/VideoStream.js
@@ -56,7 +56,7 @@ class VideoStream {
   }
 
   joinChannel = (roomId, userId) => {
-    this.client.join('006a79d3d6b148340be8c8375ea556f824cIABtQcMDK9gGwgfNb/PzHRsWjXAHRlwoLzUPbHmDqLPBQAx+f9gAAAAAEABID2Uqo1K1XwEAAQCjUrVf', roomId, userId, (uid) => {
+    this.client.join('006a79d3d6b148340be8c8375ea556f824cIAAkc5+JgIeE6maxUX0HELhWlSl7Vmc+bYClpvwBHJyhigx+f9gAAAAAEABC9EaCBGzXXwEAAQD3a9df', roomId, userId, (uid) => {
       const logMessage = 'User ' + uid + ' join channel successfully'
       console.log(logMessage)
       this.client.publish(this.localStream, err => {
