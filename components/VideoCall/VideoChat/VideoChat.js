b255d79f05b8f1dd06d3e63094f1fa7ca8eb7ea1
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-12-19 18:48:35 +0530

    fix agora errors

--- components/VideoCall/VideoChat/VideoChat.js
+++ components/VideoCall/VideoChat/VideoChat.js
@@ -2,33 +2,36 @@ import React from 'react'
 
 import Message from '../../Chat/Message'
 
-const VideoChat = () => (
-  <div className='m-4 mt-6 w-full'>
-    <Message
-    type='text'
-      author={{ name: 'learner', isSelf: false, profilePic: 'videocall.png' }}
-      content='We cant hear u can speak louder please'
-      time='5 min ago'
-    />
-    <Message
-    type='text'
-      author={{ name: 'learner', isSelf: false, profilePic: 'videocall.png' }}
-      content='Please dont spam here'
-      time='5 min ago'
-    />
-    <Message
-    type='text'
-      author={{ name: 'learner', isSelf: false, profilePic: 'videocall.png' }}
-      content='We cant hear u can speak louder please'
-      time='5 min ago'
-    />
-    <Message
-    type='text'
-      author={{ name: 'learner', isSelf: false, profilePic: 'videocall.png' }}
-      content='We cant hear u can speak louder please'
-      time='5 min ago'
-    />
-  </div>
-)
+const VideoChat = () => {
+  let time = new Date()
+  return (
+    <div className="m-4 mt-6 w-full">
+      <Message
+        type="text"
+        author={{ name: 'learner', isSelf: false, profilePic: 'videocall.png' }}
+        content="We cant hear u can speak louder please"
+        time={time}
+      />
+      <Message
+        type="text"
+        author={{ name: 'learner', isSelf: false, profilePic: 'videocall.png' }}
+        content="Please dont spam here"
+        time={time}
+      />
+      <Message
+        type="text"
+        author={{ name: 'learner', isSelf: false, profilePic: 'videocall.png' }}
+        content="We cant hear u can speak louder please"
+        time={time}
+      />
+      <Message
+        type="text"
+        author={{ name: 'learner', isSelf: false, profilePic: 'videocall.png' }}
+        content="We cant hear u can speak louder please"
+        time={time}
+      />
+    </div>
+  )
+}
 
 export default VideoChat
