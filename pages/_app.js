f9d773cc7d2a3261883dd376c7cff1338172458c
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-01-14 15:32:42 +0530

    Change Date-Time picker

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,12 @@ import Header from '../components/Header'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
 import { useEffect } from 'react'
+// import ("react-calendar/dist/Calendar.css");
+import '../styles/Calendar.css'
+// import ("react-clock/dist/Clock.css");
+import '../styles/Clock.css'
+// import ("./DateTimePicker.css");
+import '../styles/DateTimePicker.css'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
--- views/Expert/CreateService/ScheduleSelector.js
+++ views/Expert/CreateService/ScheduleSelector.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import DatePicker from '../../../components/DateAndTimePicker/DatePicker/DatePicker'
+// import DatePicker from '../../../components/DateAndTimePicker/DatePicker/DatePicker'
+import DateTimePicker from 'react-datetime-picker'
 import Pills from '../../../components/Misc/3Pills/3Pills'
 import ServiceFreeqSel from './LiveServiceFormComponents/ServiceFreeqSelector/ServiceFreeqSel'
 import DaySelector from './LiveServiceFormComponents/DaySelector/DaySelector'
@@ -36,7 +37,7 @@ const ScheduleSelector = ({
     return (
       <React.Fragment>
         <ServiceFreeqSel activeLabel={serviceFreq} setLabel={setServiceFreq} />
-        <div className="mt-6 grid grid-cols-2">
+        <div className="mt-6 grid grid-rows-2 md:grid-rows-1 md:grid-cols-2">
           <div className="w-10/12">
             <DaySelector
               changedHandler={(value) => {
@@ -52,31 +53,41 @@ const ScheduleSelector = ({
         </div>
         <div className="mt-6">
           {/* <DatePicker /> */}
-          <div className="w-full flex text-lg font-medium">
-            <div className="w-6/12">
+          <div className="w-full grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 gap-4 text-lg font-medium">
+            <div className="w-full">
               <p className="mb-2">Start date</p>
-              <div className="-ml-2">
-                <DatePicker
+              <div className="w-full">
+                {/* <DatePicker
                   // value={startDate}
 
                   handleDayChange={startDateChangeHandler}
+                /> */}
+                <DateTimePicker
+                  className="w-10/12 bg-lightGrey border-0 h-10"
+                  value={startDate}
+                  onChange={startDateChangeHandler}
                 />
               </div>
             </div>
-            <div className="w-6/12">
+            <div className="w-full">
               <p className="mb-2">End date</p>
-              <div className="-ml-2">
-                <DatePicker
+              <div className="w-full">
+                {/* <DatePicker
                   // value={endDate}
 
                   handleDayChange={endDateChangeHandler}
+                /> */}
+                <DateTimePicker
+                  className="w-10/12 bg-lightGrey border-0 h-10"
+                  value={endDate}
+                  onChange={endDateChangeHandler}
                 />
               </div>
             </div>
           </div>
         </div>
-        <div className="flex mt-6">
-          <div className="w-6/12">
+        <div className="w-full grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 gap-4 mt-6">
+          {/* <div className="w-full">
             <TimeSelector
               hourValue={hour}
               minValue={min}
@@ -90,8 +101,8 @@ const ScheduleSelector = ({
                 minChangedHandler(value)
               }}
             />
-          </div>
-          <div className="w-6/12">
+          </div> */}
+          <div className="w-full">
             <DurationSelector
               duration={duration}
               onDurationChanged={(value) => {
@@ -118,7 +129,7 @@ const ScheduleSelector = ({
           <Pills
             activeLabel={paymentFreq}
             setLabel={setPaymentFreq}
-            width="w-6/12"
+            width="w-full"
             color="white"
             label1="Per day"
             label2="Per week"
@@ -126,22 +137,32 @@ const ScheduleSelector = ({
           />
         </div>
 
-        <div className="mt-6 w-full flex text-lg font-medium">
-          <div className="w-6/12">
+        <div className="mt-6 w-full grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 gap-4 text-lg font-medium">
+          <div className="w-full">
             <p className="mb-2">Start date</p>
-            <div className="-ml-2">
-              <DatePicker
+            <div className="w-full">
+              {/* <DatePicker
                 value={startDate}
                 handleDayChange={startDateChangeHandler}
+              /> */}
+              <DateTimePicker
+                className="w-10/12 bg-lightGrey border-0 h-10"
+                value={startDate}
+                onChange={startDateChangeHandler}
               />
             </div>
           </div>
-          <div className="w-6/12">
+          <div className="w-full">
             <p className="mb-2">End date</p>
-            <div className="-ml-2">
-              <DatePicker
+            <div className="w-full">
+              {/* <DatePicker
                 value={endDate}
                 handleDayChange={endDateChangeHandler}
+              /> */}
+              <DateTimePicker
+                className="w-10/12 bg-lightGrey border-0 h-10"
+                value={endDate}
+                onChange={endDateChangeHandler}
               />
             </div>
           </div>
